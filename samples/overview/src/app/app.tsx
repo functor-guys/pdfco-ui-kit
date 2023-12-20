12e22153fdd5049e50a7a8439d9bc212b485dbd7
Luan Tran <luantran@Luans-MacBook-Pro-2.local>
2023-12-21 00:21:24 +0700

    add samples

---
+++ samples/overview/src/app/app.tsx
@@ -0,0 +1,13 @@
+import NxWelcome from './nx-welcome';
+import { Button } from '@functor-guys/ui-kit-common';
+
+export function App() {
+  return (
+    <div>
+      <Button>ahaha</Button>
+      <NxWelcome title="overview" />
+    </div>
+  );
+}
+
+export default App;
